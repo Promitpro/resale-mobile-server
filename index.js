fc09594f4b17b82266f540246a4bf1308ccc4ed7
promit-purkayastha <promitpurkayastha2022@gmail.com>
2023-07-08 12:13:58 +0600

    users collection created

--- index.js
+++ index.js
@@ -30,39 +30,58 @@ async function run() {
     const productCollection = client.db('resaleMobile').collection('products');
     const bookingsCollection = client.db('resaleMobile').collection('bookings');
     const sellingProductCollection = client.db('resaleMobile').collection('sellingProducts');
+    const usersCollection = client.db('resaleMobile').collection('users');
 
-    app.get('/products', async(req, res) => {
-        const query = {};
-        const product = await productCollection.find(query).toArray();
-        res.send(product);
+    app.get('/products', async (req, res) => {
+      const query = {};
+      const product = await productCollection.find(query).toArray();
+      res.send(product);
     })
-    app.get('/products/:id', async(req,res) => {
-        const productId = req.params.id;
-        const query = {_id: new ObjectId(productId)};
-        const product = await productCollection.findOne(query);
-        // const productsArray = product.products; 
-        res.send(product);
+    app.get('/products/:id', async (req, res) => {
+      const productId = req.params.id;
+      const query = { _id: new ObjectId(productId) };
+      const product = await productCollection.findOne(query);
+      // const productsArray = product.products; 
+      res.send(product);
     })
-    app.post('/bookings', async(req, res) => {
+    app.post('/bookings', async (req, res) => {
       const booking = req.body;
-      
+
       const result = await bookingsCollection.insertOne(booking);
       res.send(result);
     })
-    app.post('/sellingProducts', async(req, res) => {
+    app.get('/sellingProducts', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const myProducts = await sellingProductCollection.find(query).toArray();
+      res.send(myProducts)
+    })
+    app.post('/sellingProducts', async (req, res) => {
       const sellingProduct = req.body;
       const result = await sellingProductCollection.insertOne(sellingProduct);
       res.send(result);
     })
-  } finally {
     
+    app.post('/users', async (req, res) => {
+      const { name, email, userType } = req.body;
+      const existingUser = await usersCollection.findOne({ email });
+      if (existingUser) {
+        console.log('User already exists');
+        return;
+      }
+      const user = { name, email, userType };
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+  } finally {
+
   }
 }
 run().catch(console.dir);
 
 
-app.get('/', async(req, res) => {
-    res.send('resale mobile server is running')
+app.get('/', async (req, res) => {
+  res.send('resale mobile server is running')
 })
 
-app.listen(port, ()=> console.log(`Resale mobile running on ${port}`))
+app.listen(port, () => console.log(`Resale mobile running on ${port}`))
